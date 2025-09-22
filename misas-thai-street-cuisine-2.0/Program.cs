289dd768caf18885d7e722fe87be8ed0cf5907c1
Adam Wentz <adam.k.wentz@gmail.com>
2025-09-22 14:43:13 -0400

    Home page updates

--- misas-thai-street-cuisine-2.0/Program.cs
+++ misas-thai-street-cuisine-2.0/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using misas_thai_street_cuisine_2._0;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +11,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<misas_thai_street_cuisine_2._0.Services.ShoppingCartService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register Blazorise services
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
