c193b9595e484b6547db9efda265f7206f900638
Adam Wentz <adam.k.wentz@gmail.com>
2025-09-25 20:21:26 -0400

    Fixed Order Now button.

--- misas-thai-street-cuisine-2.0/wwwroot/js/site.js
+++ misas-thai-street-cuisine-2.0/wwwroot/js/site.js
@@ -2,3 +2,8 @@ window.preloadImage = function (url) {
     var img = new Image();
     img.src = url;
 };
+
+window.scrollToMenuInfo = function() {
+    var el = document.getElementById('menu-info');
+    if (el) el.scrollIntoView({ behavior: 'smooth' });
+};
