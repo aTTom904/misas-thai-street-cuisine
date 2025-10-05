4ec8708f884c05baae139298ad05884a40c3b6f1
Adam Wentz <adam.k.wentz@gmail.com>
2025-10-05 14:39:02 -0400

    Refactor: Add missing using directives and remove staticwebapp.config.json

--- api/TakePayment.cs
+++ api/TakePayment.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
